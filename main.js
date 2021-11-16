ac559125fecd3c1a9375137fb93aa5e9ace8b856
gum1900 <72326772+gum1900@users.noreply.github.com>
2021-11-16 16:44:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-import * as Square from './modules/square.js';
+import { Square } from './modules/square.js';
 import * as Canvas from './modules/canvas.js';
 
 let myCanvas = Canvas.create('myCanvas', document.body, 480, 320);
 let reportList = Canvas.createReportList(myCanvas.id);
 
-let square1 = Square.draw(myCanvas.ctx, 50, 75, 100, 'blue');
+let sq1 = new Square(myCanvas.ctx, 50, 75, 100, 'blue')
+let square1 = sq1.draw();
 Square.reportArea(square1.length, reportList);
 Square.reportPerimeter(square1.length, reportList);
