ab3b3aa7dfcb0d8db4bbacc5a8d410c131ec8752
gum1900 <72326772+gum1900@users.noreply.github.com>
2021-11-16 16:42:06 +0530

    Update square.js

--- modules/square.js
+++ modules/square.js
@@ -1,27 +1,33 @@
-const name = 'square';
+class Square {
+    const name = 'square';
+    let ctx, length, x, y, color;
+    constructor(ctx, length, x, y, color) {
+        {this.ctx, this.length, this.x. this.y, this.color} = {ctx, length, x, y, color};
+    }
 
-function draw(ctx, length, x, y, color) {
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, length, length);
-	return {length: length, x: x, y: y, color: color};
+    function draw() {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, length, length);
+        return {length: length, x: x, y: y, color: color};
+    }
+
+    function reportArea(length, listID) {
+        let list = document.querySelector("ul#" + listID);
+        if(list) {
+            let li = document.createElement("li");
+            li.textContent = "Area of Square with side " + length + " is " + (length*length) + ".";
+            list.append(li);
+        }
+    }
+
+    function reportPerimeter(length, listID) {
+        let list = document.querySelector("ul#" + listID);
+        if(list) {
+            let li = document.createElement("li");
+            li.textContent = "Perimeter of Square with side " + length + " is " + (4*length) + ".";
+            list.append(li);
+        }
+    }
 }
 
-function reportArea(length, listID) {
-	let list = document.querySelector("ul#" + listID);
-	if(list) {
-		let li = document.createElement("li");
-		li.textContent = "Area of Square with side " + length + " is " + (length*length) + ".";
-		list.append(li);
-	}
-}
-
-function reportPerimeter(length, listID) {
-	let list = document.querySelector("ul#" + listID);
-	if(list) {
-		let li = document.createElement("li");
-		li.textContent = "Perimeter of Square with side " + length + " is " + (4*length) + ".";
-		list.append(li);
-	}
-}
-
-export {name, draw, reportArea, reportPerimeter};
+export { Square };
